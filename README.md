787e37ffbc37c1775026bc10b8faadc7cb5e38b7
John Arundel <john@bitfieldconsulting.com>
2024-12-18 16:25:10 +0000

    add time setting

---
+++ README.md
@@ -0,0 +1,7 @@
+# `microbit-clock`
+
+A simple clock application for the BBC micro:bit. Deploy it to your connected micro:bit with:
+
+**`cargo run`**
+
+The micro:bit should start showing the time as '00:00'. Press button A to advance the hours, or button B to advance the minutes, to set the current time.
--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,63 @@
 use core::str::from_utf8;
 
 use embassy_executor::Spawner;
-use embassy_futures::select::{select, Either};
+use embassy_futures::select::{select3, Either3};
 use embassy_time::Duration;
 use microbit_bsp::{display, Microbit};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(#[allow(clippy::used_underscore_binding)]_spawner: Spawner) {
     let board = Microbit::default();
     let mut btn_a = board.btn_a;
     let mut btn_b = board.btn_b;
     let mut display = board.display;
 
     display.set_brightness(display::Brightness::MAX);
-    let mut hours = 13;
-    let mut minutes = 1;
-    let mut buf = [0u8; 5];
+    let mut hours = 0;
+    let mut minutes = 0;
+    let mut seconds = 0;
+    let mut buf = [0u8; 6];
     loop {
+        if seconds == 60 {
+            seconds = 0;
+            minutes += 1;
+            if minutes == 60 {
+                minutes = 0;
+                hours = (hours + 1) % 24;
+            }
+        }
         format_time(&mut buf, hours, minutes);
         let time_str = from_utf8(&buf).expect("bytes should be valid UTF-8");
-        display.scroll_with_speed(time_str, Duration::from_millis(2000)).await;
-        // match select(btn_a.wait_for_low(), btn_b.wait_for_low()).await {
-        //     Either::First(()) => {
-        //         defmt::info!("A pressed");
-        //         hours = (hours + 1) % 24;
-        //     },
-        //     Either::Second(()) => {
-        //         defmt::info!("B pressed");
-        //         minutes = (minutes + 1) % 60;
-        //     },
-        // }
+        defmt::info!("Time {:02}:{:02}:{:02}", hours, minutes, seconds);
+        match select3(
+            display.scroll_with_speed(time_str, Duration::from_millis(2000)),
+            btn_a.wait_for_low(),
+            btn_b.wait_for_low(),
+        )
+        .await
+        {
+            Either3::First(()) => {
+                seconds += 2;
+            }
+            Either3::Second(()) => {
+                hours = (hours + 1) % 24;
+                btn_a.wait_for_high().await;
+            }
+            Either3::Third(()) => {
+                minutes = (minutes + 1) % 60;
+                seconds = 0;
+                btn_b.wait_for_high().await;
+            }
+        }
     }
 }
 
-
 fn format_time(buf: &mut [u8], hours: u8, minutes: u8) {
-    buf[0] = b'0' + hours / 10;
-    buf[1] = b'0' + hours % 10;
-    buf[2] = b':';
-    buf[3] = b'0' + minutes / 10;
-    buf[4] = b'0' + minutes % 10;
+    buf[0] = b' ';
+    buf[1] = b'0' + hours / 10;
+    buf[2] = b'0' + hours % 10;
+    buf[3] = b':';
+    buf[4] = b'0' + minutes / 10;
+    buf[5] = b'0' + minutes % 10;
 }
